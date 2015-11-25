9fe3d0d448fd65356689fe6953b31b7ddff1722f
Jon Ross <jonross.zlsa@gmail.com>
2015-11-25 14:29:39 -0800

    version 1.0 alpha 1, with example

--- fishfingers.js
+++ fishfingers.js
@@ -1,6 +1,22 @@
 
+// FISHFINGERS.JS
+
+// SIMPLER ALERT() WITH LESS KEYSTROKES
+
+// CC0
+
+function alrt(message) {
+  window.alert(message + '\nBrought to you by fishfingers.js, the premier keystroke-saving JS library');
+}
+
 var ff = {};
 
 ff.alert = function(message) {
-  window.alert(message + '\nBrought to you by fishfingers.js');
+  alrt(message + '\nNOTE: ff.alert() is DEPRECATED! Do not use it in future projects!');
+}
+
+// make sure users are using jquery
+
+if(!window.jQuery) {
+  alrt('fishfingers.js requires jQuery to be included first!');
 }
